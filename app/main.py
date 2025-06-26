867e624f473a472e1d9ac44a723e6e128aab37a2
jcuenod <4253884+jcuenod@users.noreply.github.com>
2025-06-26 15:12:07 -0500

    module

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ from typing import Optional, Dict, Any, List
 from dataclasses import dataclass
 from enum import Enum
 
-from app.templates.align import create_align_config_for
+from .templates.align import create_align_config_for
 
 from .env import SILAUTO_URL, SILNLP_ROOT, CUDA_DEVICE
 
