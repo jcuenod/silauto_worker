d52d5e99e1f50d711011f02ddbf76033f55a1da2
jcuenod <4253884+jcuenod@users.noreply.github.com>
2025-07-25 08:13:41 -0500

    Fix sfm glob path

--- app/task_scripts/translate.py
+++ app/task_scripts/translate.py
@@ -2,6 +2,13 @@ from typing import List
 from app.env import CUDA_DEVICE, SILNLP_EXPERIMENTS_ROOT, SILNLP_ROOT, USFM2PDF_PATH
 from app.models import TaskStatus
 
+def _get_usfm_generate_script(experiment_name: str, source_project_id: str):
+    path_to_sfms = SILNLP_EXPERIMENTS_ROOT / experiment_name / "infer/*" / source_project_id / "*.SFM"
+    return f"""
+        # Try to generate PDFs for the drafts, but ignore any failure
+        cd $USFM2PDF_PATH
+        python main.py -n --header 'AI Draft' '{path_to_sfms}'"""
+
 
 def generate_translate_script(
     task_id: str,
@@ -14,15 +21,6 @@ def generate_translate_script(
 ) -> str:
     """Generate script content for translation task"""
     books_str = ",".join(book_names)
-
-    usfm_generate_script = (
-        f"""
-        # Try to generate PDFs for the drafts, but ignore any failure
-        cd $USFM2PDF_PATH
-        python main.py -n --header 'AI Draft' '{SILNLP_EXPERIMENTS_ROOT}{experiment_name}/infer/*/{source_project_id}/*.SFM'"""
-        if USFM2PDF_PATH
-        else ""
-    )
 
     return f"""
 # Translate task for (train) project: {train_task_id}
@@ -51,8 +49,8 @@ CUDA_VISIBLE_DEVICES={CUDA_DEVICE} screen -L -d -m -S "$SESSION_NAME" bash -c "
     exec > >(tee -a $LOG_FILE) 2>&1
     echo 'Starting translation process...'
     if poetry run python -m silnlp.nmt.translate {experiment_name} --src-project {source_project_id} --books {books_str} --src-iso {source_script_code} --trg-iso {target_script_code} --checkpoint best; then
-        {usfm_generate_script}
         echo 'SUCCESS' > $STATUS_FILE
+        {_get_usfm_generate_script(experiment_name, source_project_id) if USFM2PDF_PATH else ""}
     else
         echo 'FAILED' > $STATUS_FILE
     fi
